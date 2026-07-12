aeaae917f1c65b5b39e8b25afc33ab54b4c6ad86
saurabh-trivedi-dev <saurabhtrivedi.dev@gmail.com>
2026-07-12 12:30:42 +0530

    promises

--- promises.js
+++ promises.js
@@ -44,9 +44,13 @@ promise3
 
 
 const promise4 = new Promise((resolve, reject) => {
+
     console.log("Inside Promise");
+
     resolve("Done");
+
     console.log("After Resolve");
+
 });
 
 console.log("Outside Promise");
